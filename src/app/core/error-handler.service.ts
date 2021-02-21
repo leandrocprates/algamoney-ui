a446eab9a47505a8d76d9e49d8535fb57173644e
leandro prates <leandro.prates@gmail.com>
2021-02-21 12:45:35 -0300

    melhorias mensagem de error

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MessageService } from 'primeng/api';
 
@@ -16,11 +17,22 @@ export class ErrorHandlerService {
 
     if ( errorResponse.error instanceof ErrorEvent) {
       msg = errorResponse.error.message ;
+    } else if ( errorResponse instanceof HttpErrorResponse
+        && errorResponse.status >=400 &&  errorResponse.status <=499 ) {
+
+          const message = errorResponse.message ;
+          const status = errorResponse.status ;
+
+          try{
+            const mensagemUsuario = errorResponse.error[0].mensagemUsuario ;
+            msg = mensagemUsuario ;
+          }catch(Exception){}
+
     } else {
       msg = `Backend returned code ${errorResponse.status},  body was: ${errorResponse.message}` ;
     }
 
-    this.messageService.add({severity:'error', summary: 'Error', detail: msg });
+    this.messageService.add({severity:'error', summary: 'Erro', detail: msg });
   }
 
 }

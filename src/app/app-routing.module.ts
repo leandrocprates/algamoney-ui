45df75060adc1a1d1ee3245f2a01649f288220c6
leandro prates <leandro.prates@gmail.com>
2021-03-15 17:55:01 -0300

    colocando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { PessoasCadastroComponent } from './pessoas/pessoas-cadastro/pessoas-cadastro.component';
+import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
+import { LancamentosCadastroComponent } from './lancamentos/lancamentos-cadastro/lancamentos-cadastro.component';
+import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'lancamentos' , component : LancamentosPesquisaComponent},
+  {path : 'lancamentos/novo' , component : LancamentosCadastroComponent },
+  {path : 'pessoas' , component :  PessoasPesquisaComponent },
+  {path : 'pessoas/novo' , component :  PessoasCadastroComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
